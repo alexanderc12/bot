ab426b5bbac77a45b532697883bea3f02e693b07
Alexander <alexanderc@outlook.com>
2016-09-25 17:23:41 -0500

    Basic Bot response and initial code for consume a web service.

--- src/models/Bot.java
+++ src/models/Bot.java
@@ -1,5 +1,51 @@
 package models;
 
-public class Bot {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class Bot extends TelegramLongPollingBot{
+	
+	public static final String BOT_USERNAME = "ALEX_2016_BOT";
+    public static final String BOT_TOKEN = "259260803:AAE0H1m9bXWQeyMS2NzbdYtP7Of50nfjNdE";
+    
+	@Override
+	public String getBotUsername() {
+		return BOT_USERNAME;
+	}
+
+	@Override
+	public void onUpdateReceived(Update update) {
+		Message message = update.getMessage();
+		if (message.hasText()) {
+			SendMessage response = new SendMessage();
+			response.setChatId(message.getChatId().toString());
+			response.setText("Hello: " + message.getFrom().getUserName());
+			try {
+                sendMessage(response);
+            } catch (TelegramApiException e) {
+            	Logger.getGlobal().log(Level.SEVERE, "Error al enviar mensaje al usuario", e);
+            }
+		}
+	}
+
+	@Override
+	public String getBotToken() {
+		return BOT_TOKEN;
+	}
+
+	public static void main(String[] args) {
+		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+		try {
+			telegramBotsApi.registerBot(new Bot());
+		} catch (TelegramApiException e) {
+			Logger.getGlobal().log(Level.SEVERE, "Error al iniciar el bot", e);
+		}
+	}
 }
--- src/models/WebToolKit.java
+++ src/models/WebToolKit.java
@@ -1,5 +1,28 @@
 package models;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
 public class WebToolKit {
 
+	private static final String USER_AGENT_PROPERTY = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36";
+	private static final String USER_AGENT = "User-Agent";
+
+	public static String getPageCode(String siteURL) throws IOException{
+		URL url = new URL(siteURL);
+		URLConnection connection = url.openConnection();
+		connection.addRequestProperty(USER_AGENT, USER_AGENT_PROPERTY);
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+		String inputLine;
+		StringBuilder code = new StringBuilder();
+		while ((inputLine = buffer.readLine()) != null){
+			code.append(inputLine);
+		}
+		buffer.close();
+		return code.toString();
+	}
 }
